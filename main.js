9cb89ac20daee641fe63d324fcc62f0da11f0575
BrunoCostantin <brunocostantin@gmail.com>
2023-04-12 22:37:48 -0300

    fix exercicio

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-let botao = document.getElementById("btn-enviar");
+const form = document.getElementById("form-deposito")
+const botao = document.getElementById("btn-enviar");
 botao.disable = true
 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+})
+
 function validaFormulario() {
-    let mes = document.getElementById("mes-nascimento").value;
-    let ano = document.getElementById("ano-nascimento").value;
+    let mes = document.getElementById("mes-nascimento").value
+    let ano = document.getElementById("ano-nascimento").value
 
     if (mes > ano) {
         alert("Você preencheu o mês de nascimento maior que o ano, preencha novamente");
         return false;
     } else {
-        alert("Você nasceu no mês", mes," e no ano ", ano,);
+        alert("Você nasceu no mês " + mes + " e no ano " + ano)
         return true;
     }
 }
+
+console.log (form)
